4e73862f28c3427baec300abb4834ce1d0f2efdd
Casper van Battum <cvanbattum@gmail.com>
2020-02-14 14:06:23 +0100

    Astar solution

--- Astar/Assets/Scripts/Astar.cs
+++ Astar/Assets/Scripts/Astar.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-public class Astar
-{
+public class Astar {
     /// <summary>
     /// TODO: Implement this function so that it returns a list of Vector2Int positions which describes a path
     /// Note that you will probably need to add some helper functions
@@ -14,9 +14,107 @@ public class Astar
     /// <param name="endPos"></param>
     /// <param name="grid"></param>
     /// <returns></returns>
-    public List<Vector2Int> FindPathToTarget(Vector2Int startPos, Vector2Int endPos, Cell[,] grid)
-    {
-        return null;
+    public List<Vector2Int> FindPathToTarget(Vector2Int startPos, Vector2Int endPos, Cell[,] grid) {
+        var nodeGrid = CreateNodeGrid(grid, endPos);
+        
+        var openList = new List<Node>();
+        var closedList = new List<Node>();
+
+        // Create a node from the starting cell
+        var startNode = nodeGrid[startPos.x, startPos.y];
+        startNode.GScore = 0;
+        openList.Add(startNode);
+
+        while (openList.Count > 0) {
+            // FIND NODE WITH LOWEST FSCORE
+            if (openList.Count > 1) openList = openList.OrderBy(node => node.FScore).ToList();
+            var current = openList[0];
+
+            // CHECK IF END REACHED
+            if (current.position == endPos) {
+                return BacktrackPositions(current, startNode);
+            }
+
+            // VISIT CURRENT NODE
+            closedList.Add(current);
+            openList.Remove(current);
+
+            // GET NEIGHBOUR NODES
+            var neighbours = new List<Node>();
+            foreach (var cell in FindAvailableNeighbours(grid[current.position.x, current.position.y], grid).ToList()) {
+                // Get nodes corresponding to neighbour cells
+                neighbours.Add(nodeGrid[cell.gridPosition.x, cell.gridPosition.y]);
+            }
+            
+            foreach (var node in neighbours) {
+                if (closedList.Contains(node)) {
+                    continue;
+                }
+
+                var tentativeG = current.GScore + 1;
+
+                if (tentativeG < node.GScore) {
+                    node.parent = current;
+                    node.GScore = tentativeG;
+                    if (!openList.Contains(node)) {
+                        openList.Add(node);
+                    }
+                }
+            }
+        }
+
+        return new List<Vector2Int>();
+    }
+
+    private List<Vector2Int> BacktrackPositions(Node start, Node end) {
+        var positions = new List<Vector2Int>();
+
+        var currentNode = start;
+        do {
+            positions.Add(currentNode.position);
+            currentNode = currentNode.parent;
+        } while (currentNode != end);
+
+        positions.Reverse();
+        return positions;
+    }
+
+    private Node[,] CreateNodeGrid(Cell[,] cellGrid, Vector2Int endPos) {
+        var nodeGrid = new Node[cellGrid.GetLength(0), cellGrid.GetLength(1)];
+        
+        foreach (var cell in cellGrid) {
+            var pos = cell.gridPosition;
+            nodeGrid[pos.x, pos.y] = new Node(pos, null, Int32.MaxValue, CalcHScore(pos, endPos));
+        }
+
+        return nodeGrid;
+    }
+
+    private IEnumerable<Cell> FindAvailableNeighbours(Cell cell, Cell[,] grid) {
+        var neighbours = new List<Cell>();
+
+        if (!cell.HasWall(Wall.UP)) {
+            neighbours.Add(grid[cell.gridPosition.x, cell.gridPosition.y + 1]);
+        }
+
+        if (!cell.HasWall(Wall.DOWN)) {
+            neighbours.Add(grid[cell.gridPosition.x, cell.gridPosition.y - 1]);
+        }
+
+        if (!cell.HasWall(Wall.LEFT)) {
+            neighbours.Add(grid[cell.gridPosition.x - 1, cell.gridPosition.y]);
+        }
+
+        if (!cell.HasWall(Wall.RIGHT)) {
+            neighbours.Add(grid[cell.gridPosition.x + 1, cell.gridPosition.y]);
+        }
+
+        return neighbours;
+    }
+
+    private static int CalcHScore(Vector2Int from, Vector2Int to) {
+        // Using manhattan distance as heuristic
+        return Mathf.Abs(to.x - from.x) + Mathf.Abs(to.y - from.y);
     }
 
     /// <summary>
